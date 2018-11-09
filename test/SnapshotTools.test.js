4d6b1be1366ce7f5892aed3409cf87fad1fe0a25
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-11-09 07:09:37 +0100

    test for parsing one csv line

--- test/SnapshotTools.test.js
+++ test/SnapshotTools.test.js
@@ -1,11 +1,12 @@
 const SnapshotTools = require('../services/SnapshotTools');
 const assert = require('assert');
 
-describe('parsing csv', function() {
-  describe('one line', () => {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1,2,3].indexOf(4), -1);
+describe('parsing csv', () => {
+    it('parce one line', () => {
+        const csv = "\"0x00000000000000000000000000000000000000b1\",\"b1\",\"EOS5cujNHGMYZZ2tgByyNEUaoPLFhZVmGXbZc9BLJeQkKZFqGYEiQ\",\"100000000.0100\"";
+        const result = SnapshotTools.csvToJson(csv);
+        assert.equal(result[0].account, 'b1');
+        assert.equal(result[0].amount, '100000000.0100');
     });
-  });
 });
 
